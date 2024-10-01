98c89129ba6f8967bf4c245104b6d9eb7d135c1f
Yagoo Pereira <yagoo.pereira@alphabeto.com>
2024-10-01 18:49:27 -0300

    Subindo primeiros arquivos

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'primereact/resources/themes/lara-light-cyan/theme.css';
+import 'primereact/resources/primereact.min.css'; // Importa os estilos padrão
+import 'primeicons/primeicons.css'; // Importa os ícones
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
