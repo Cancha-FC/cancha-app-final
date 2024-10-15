dd727687145dc5f2d1a2ee75763b96ed800dbd28
Yagoo Pereira <yagoo.pereira@alphabeto.com>
2024-10-15 19:45:25 -0300

    Ajustando ranking e filterbar data

--- src/Components/FilterBar/FilterBar.js
+++ src/Components/FilterBar/FilterBar.js
@@ -6,18 +6,30 @@ import { InputText } from 'primereact/inputtext';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { Checkbox } from 'primereact/checkbox';
+import { addLocale } from 'primereact/api';
 import './FilterBar.css'; // Estilos personalizados
+
+addLocale('pt-BR', {
+    firstDayOfWeek: 0,
+    dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
+    dayNamesShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'],
+    dayNamesMin: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'],
+    monthNames: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+    monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+    today: 'Hoje',
+    clear: 'Limpar',
+});
 
 const FilterBar = ({ onFilter }) => {
     const [startDate, setStartDate] = useState(null);
     const [endDate, setEndDate] = useState(null);
-    const [selectedLicensees, setSelectedLicensees] = useState([]); // Armazena os licenciados selecionados
-    const [licensees, setLicensees] = useState([]); // Estado para os licenciados
-    const [searchTerm, setSearchTerm] = useState(''); // Estado para o campo de pesquisa
-    const [modalVisible, setModalVisible] = useState(false); // Controle de visibilidade do modal
+    const [selectedLicensees, setSelectedLicensees] = useState([]); 
+    const [licensees, setLicensees] = useState([]); 
+    const [searchTerm, setSearchTerm] = useState(''); 
+    const [modalVisible, setModalVisible] = useState(false); 
+    const [dateModalVisible, setDateModalVisible] = useState(false); 
 
     useEffect(() => {
-        // Função para buscar licenciados da API
         const fetchLicenciados = async () => {
             try {
                 const response = await fetch('http://127.0.0.1:8000/api/licenciados/', {
@@ -32,10 +44,9 @@ const FilterBar = ({ onFilter }) => {
             }
         };
 
-        fetchLicenciados();  // Chama a função ao montar o componente
+        fetchLicenciados();
     }, []);
 
-    // Função para controlar a seleção de licenciados
     const onLicenseeSelect = (e, licenciado) => {
         const selected = [...selectedLicensees];
         if (e.checked) {
@@ -47,56 +58,38 @@ const FilterBar = ({ onFilter }) => {
         setSelectedLicensees(selected);
     };
 
-    // Função chamada ao clicar no botão "Buscar"
     const handleSearch = () => {
         onFilter({ startDate, endDate, selectedLicensees });
     };
 
-    // Filtro para o campo de pesquisa
-    const filteredLicensees = licensees.filter((licenciado) => 
+    const filteredLicensees = licensees.filter((licenciado) =>
         licenciado.nome.toLowerCase().includes(searchTerm.toLowerCase())
     );
 
-    // Exibe o nome dos licenciados selecionados ou um resumo
     const getSelectedLicenseesLabel = () => {
         if (selectedLicensees.length === 1) {
-            return selectedLicensees[0].nome; // Exibe o nome do único licenciado selecionado
+            return selectedLicensees[0].nome; 
         } else if (selectedLicensees.length > 1) {
-            return `${selectedLicensees.length} licenciados selecionados`; // Exibe "X licenciados selecionados"
+            return `${selectedLicensees.length} licenciados selecionados`; 
         } else {
-            return "Selecionar Licenciados"; // Texto padrão se nenhum licenciado for selecionado
+            return "Selecionar Licenciados"; 
         }
+    };
+
+    // Função para formatar as datas no formato "dd/mm/yyyy a dd/mm/yyyy"
+    const getFormattedDateRange = () => {
+        if (startDate && endDate) {
+            const formattedStartDate = new Date(startDate).toLocaleDateString('pt-BR');
+            const formattedEndDate = new Date(endDate).toLocaleDateString('pt-BR');
+            return `${formattedStartDate} a ${formattedEndDate}`;
+        }
+        return 'Selecionar Período';
     };
 
     return (
         <div className="filter-bar">
-            {/* DatePicker para data de início */}
-            <div className="calendar-container">
-                <Calendar 
-                    id="start"
-                    value={startDate} 
-                    onChange={(e) => setStartDate(e.value)} 
-                    placeholder="Selecione a data de início" 
-                    showIcon 
-                    touchUI={false} 
-                    locale="pt-BR" 
-                    dateFormat="dd/mm/yy"
-                />
-            </div>
-
-            {/* DatePicker para data de fim */}
-            <div className="calendar-container">
-                <Calendar 
-                    id="end"
-                    value={endDate} 
-                    onChange={(e) => setEndDate(e.value)} 
-                    placeholder="Selecione a data de fim" 
-                    showIcon 
-                    touchUI={false}
-                    locale="pt-BR" 
-                    dateFormat="dd/mm/yy"
-                />
-            </div>
+            {/* Botão para abrir o modal de datas */}
+            <Button label={getFormattedDateRange()} icon="pi pi-calendar" onClick={() => setDateModalVisible(true)} />
 
             {/* Botão para abrir o modal de seleção de licenciados */}
             <div className="dropdown-licenciados">
@@ -106,27 +99,61 @@ const FilterBar = ({ onFilter }) => {
             {/* Botão de busca */}
             <Button label="Buscar" icon="pi pi-search" onClick={handleSearch} />
 
+            {/* Modal para selecionar o período de datas */}
+            <Dialog header="Período de consulta" visible={dateModalVisible} onHide={() => setDateModalVisible(false)} style={{ width: '40vw' }}>
+                <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+                    <div className="calendar-container">
+                        <label htmlFor="start">Início</label>
+                        <Calendar
+                            id="start"
+                            value={startDate}
+                            onChange={(e) => setStartDate(e.value)}
+                            placeholder="Selecione a data de início"
+                            showIcon
+                            touchUI={false}
+                            locale="pt-BR"
+                            dateFormat="dd/mm/yy"
+                        />
+                    </div>
+
+                    <div className="calendar-container">
+                        <label htmlFor="end">Fim</label>
+                        <Calendar
+                            id="end"
+                            value={endDate}
+                            onChange={(e) => setEndDate(e.value)}
+                            placeholder="Selecione a data de fim"
+                            showIcon
+                            touchUI={false}
+                            locale="pt-BR"
+                            dateFormat="dd/mm/yy"
+                        />
+                    </div>
+                </div>
+
+                <Button label="Confirmar" icon="pi pi-check" onClick={() => setDateModalVisible(false)} style={{ marginTop: '10px' }} />
+            </Dialog>
+
             {/* Modal para selecionar os licenciados */}
             <Dialog header="Seleção de Licenciados" visible={modalVisible} onHide={() => setModalVisible(false)} style={{ width: '50vw' }}>
-                {/* Campo de pesquisa */}
                 <span className="p-input-icon-left" style={{ marginBottom: '10px' }}>
                     <i className="pi pi-search" />
                     <InputText value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} placeholder="Pesquisar Licenciado" />
                 </span>
 
-                {/* Tabela com checkbox para selecionar licenciados */}
                 <DataTable value={filteredLicensees}>
-                    <Column body={(rowData) => (
-                        <Checkbox
-                            checked={selectedLicensees.some((l) => l.id === rowData.id)}
-                            onChange={(e) => onLicenseeSelect(e, rowData)}
-                        />
-                    )} header="Selecionar" />
+                    <Column
+                        body={(rowData) => (
+                            <Checkbox
+                                checked={selectedLicensees.some((l) => l.id === rowData.id)}
+                                onChange={(e) => onLicenseeSelect(e, rowData)}
+                            />
+                        )}
+                        header="Selecionar"
+                    />
                     <Column field="nome" header="Nome" />
-                    
                 </DataTable>
 
-                {/* Botão de confirmação */}
                 <Button label="Confirmar" icon="pi pi-check" onClick={() => setModalVisible(false)} style={{ marginTop: '10px' }} />
             </Dialog>
         </div>
