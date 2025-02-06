6dcc7d6de786245326b9baa740980d132989f7e2
Leonardo de Oliveira Miranda <85960838+leonardomiranda132@users.noreply.github.com>
2025-02-06 14:20:46 -0300

    Update index.js

--- src/Components/footer/index.js
+++ src/Components/footer/index.js
@@ -4,7 +4,7 @@ import './CardFooter.css';
 const CardFooter = () => {
     return (
         <div className="footer">
-            <p>2024 - Cancha FC - Todos os Direitos Reservados</p>
+            <p>2025 - Cancha FC - Todos os Direitos Reservados</p>
         </div>
     )
 }
